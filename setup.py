7769d007b4d44ff34b65bb4b391707001cfca0bc
Anddre Henrique <anddreh.dev@gmail.com>
2024-08-26 16:23:10 -0300

    Escondendo o terminal

--- setup.py
+++ setup.py
@@ -4,11 +4,19 @@ from cx_Freeze import setup, Executable
 
 # Definir o que deve ser incluido na pasta final
 arquivos = ['exe.ico', 'dados_usuarios.csv']
+
+# Comando para esconder console em aplicações onde não quer que o console seja exibido
+base = None
+if sys.plataform == 'Win32':
+    base = 'Win32GUI'
+
 # Saída de arquivos
 configuracao = Executable(
     script='app.py',
-    icon='exe.ico'
+    icon='exe.ico',
+    base=base
 )
+
 # Configurar o CX-Freeze (detalhes do programa)
 setup(
     name='Automatizador de Cadastro de Usuarios',
